2e2579e849cbc9fef46f7a2253f4bdf379f5ac11
Adilson <adilsonchy@gmail.com>
2025-10-19 20:52:10 -0300

    Update script.js

--- exercicio6/script.js
+++ exercicio6/script.js
@@ -3,23 +3,24 @@ const num = document.getElementById("valor");
 const soma = document.getElementById("mais");
 const menos = document.getElementById("menos");
 const reset = document.getElementById("reset");
-var cont = 0;
+
+var cont = Number(localStorage.getItem("cont"));
+num.textContent=cont; 
+fundo(cont); 
+
+
 soma.addEventListener("click", ()=>{ 
         cont+=1;
-        num.textContent=cont;  
-
-       
-        fundo(cont);
-      
+        num.textContent=cont;         
+        fundo(cont);      
       
 });
 
 menos.addEventListener("click", ()=>{ 
-       //se eu quiser numeros negativos tira o if
-    
+   
         cont-=1;
         num.textContent=cont; 
-        fundo(cont);
+        fundo(cont);      
         
         
         
@@ -36,19 +37,27 @@ reset.addEventListener("click", ()=>{
 
 
 function fundo(cont){
+
       if(cont>0){
                 num.classList.remove("vermelho")
                 num.classList.add("verde")
+            
+                localStorage.setItem("cont", `${cont}`);
+
         }
 
         if(cont<0){
                 num.classList.remove("verde")
                 num.classList.add("vermelho")
+              
+                localStorage.setItem("cont", `${cont}`);
         }
 
         if(cont===0){
                 num.classList.remove("verde")
                 num.classList.remove("vermelho")
+                
+                localStorage.setItem("cont", `${cont}`);
 
         }
 
