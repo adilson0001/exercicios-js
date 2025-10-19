16351247fbd378c50cfbfd535831413637a3e331
Adilson <adilsonchy@gmail.com>
2025-10-18 21:18:37 -0300

    contador1.2
    adicionei cores ao numero, vermelho para negativo e verde para positivo

--- exercicio6/script.js
+++ exercicio6/script.js
@@ -6,18 +6,59 @@ const reset = document.getElementById("reset");
 var cont = 0;
 soma.addEventListener("click", ()=>{ 
         cont+=1;
-        num.innerHTML=cont;  
+        num.textContent=cont;  
+
+       
+        fundo(cont);
+      
+      
 });
 
 menos.addEventListener("click", ()=>{ 
        //se eu quiser numeros negativos tira o if
-       
+    
         cont-=1;
-        num.innerHTML=cont;  
+        num.textContent=cont; 
+        fundo(cont);
+        
+        
+        
 });
 
 reset.addEventListener("click", ()=>{
     cont=0;
     num.innerHTML=cont;
+    fundo(cont);
+
+
 });
+
+
+
+function fundo(cont){
+      if(cont>0){
+                num.classList.remove("vermelho")
+                num.classList.add("verde")
+        }
+
+        if(cont<0){
+                num.classList.remove("verde")
+                num.classList.add("vermelho")
+        }
+
+        if(cont===0){
+                num.classList.remove("verde")
+                num.classList.remove("vermelho")
+
+        }
+
+
+};
+
+
+
+
+
+
+
 
