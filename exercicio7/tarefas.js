0f53882a69b458eb225f4456f4bb54d3498da37f
Adilson <adilsonchy@gmail.com>
2025-10-29 19:33:27 -0300

    exclusão de tarefas

--- exercicio7/tarefas.js
+++ exercicio7/tarefas.js
@@ -3,41 +3,36 @@ const btn = document.getElementById("btn");
 const campo = document.getElementById("campo");
 const lista = document.getElementById("lista");
 
+function addTarefa() {
+  return campo.value;
+}
 
-function addTarefa(){
-    const txt = campo.value;
-    return txt;  
-};
-
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-  
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 });
 
-btn.addEventListener("click", ()=>{
-    if(campo.value!=""){
-        lista.innerHTML+=
-    `<div class="linha">
-                    <input type="checkbox">
-                    <p>${addTarefa()}</p>
-                    <div class="icons">
-                        <p class="baixo">BAIXO</p>
-                        <img src="imagens/edit.svg" alt="icone de editar">
-                        <img src="imagens/delete.svg" alt="icone de excluir">
-                    </div>
-                </div>`;
-    campo.value="";
-    }
-    else{
-         alert("Adicione uma Tarefa!");
-    };     
-    
-
+btn.addEventListener("click", () => {
+  if (campo.value != "") {
+    lista.innerHTML += `
+      <div class="linha">
+        <input type="checkbox">
+        <p>${addTarefa()}</p>
+        <div class="icons">
+          <p class="baixo">BAIXO</p>
+          <img src="imagens/edit.svg" alt="icone de editar">
+          <img src="imagens/delete.svg" alt="icone de excluir" class="lixo">
+        </div>
+      </div>`;
+    campo.value = "";
+  } else {
+    alert("Adicione uma Tarefa!");
+  }
 });
 
-
-
-
-
-
-
+//estudar isso
+lista.addEventListener("click", (event) => {
+  if (event.target.classList.contains("lixo")) {
+    const linha = event.target.closest(".linha");
+    linha.remove();
+  }
+});
